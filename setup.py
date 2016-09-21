77b1afae22023825eafbf6b1ab8b131f454512e9
Richard Hartmann <cimatosa@gmx.de>
2016-09-22 00:15:29 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+ #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# To create a distribution package for pip or easy-install:
+# python setup.py sdist
+
+from setuptools import setup
+
+
+author = u"Richard Hartmann"
+authors = [author]
+description = 'A python progress bar in ascii art.'
+name = 'progress'
+version = '0.1'
+
+if __name__ == "__main__":
+    setup(
+        name=name,
+        author=author,
+        author_email='richard.hartmann@tu-dresden.de',
+        url='https://github.com/cimatosa/progress',
+        version=version,
+        packages=[name],
+        package_dir={name: name},
+        license="BSD (3 clause)",
+        description=description,
+        keywords=["progress", "bar", "ascii", "art"],
+        classifiers= [
+            'Operating System :: Unix',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Topic :: Environment :: Console',
+            'Intended Audience :: Developers'],
+        platforms=['ALL']
+        )
