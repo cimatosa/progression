27bf450244cbc02e0ddca17f89aff4563ddd84ed
Richard Hartmann <cimatosa@gmx.de>
2016-10-27 11:41:17 +0200

    signaling in windows implemented using mp Event class

--- tests/test_win.py
+++ tests/test_win.py
@@ -3,39 +3,43 @@ import time
 import signal
 import os
 import platform
+import threading
+import _thread
+import sys
 
-def my_h(signal, frame):
-    print("recieved signal", signal)
-    raise InterruptedError
+def interrupt_handler(interrupt_event):
+    print("before wait")
+    interrupt_event.wait()
+    print("after wait")
+    _thread.interrupt_main()
 
-def a_task():
-    signal.signal(signal.SIGINT, my_h)
+def a_task(interrupt_event, *args):
+    task = threading.Thread(target=interrupt_handler, args=(interrupt_event,))
+    task.start()
+
     print("this is 'a_task'", os.getpid())
     try:
         while True:
             print(time.time())
             time.sleep(1)
-    except Exception as e:
-        print(type(e), e)
+    except KeyboardInterrupt:
+        print("got KeyboardInterrupt")
     print("'a_task' is at end")
 
 
 if __name__ == '__main__':
-    p = mp.Process(target=a_task)
+    interrupt_event = mp.Event()
+    p = mp.Process(target=a_task, args = (interrupt_event, tuple()))
     p.start()
-    time.sleep(1)
-    
-    if platform.system() == 'Windows':
-        print("send CTRL_C_EVENT")
-        os.kill(p.pid, signal.CTRL_C_EVENT)
-    elif platform.system() == 'Linux':
-        print("send SIGINT")
-        os.kill(p.pid, signal.SIGINT)
+    time.sleep(2)
+    print("set interrupt_event")
+    interrupt_event.set()
 
     time.sleep(3)
     try:
         os.kill(p.pid, signal.SIGTERM)
     except:
         pass
+
 
 
